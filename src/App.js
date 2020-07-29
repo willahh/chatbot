40732647020beb49168a839c054c3cf801e0960a
wravel <wravel@gmail.com>
2020-07-29 21:27:20 +0200

    Avancement

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render () {
     return (
-      <div className='box' />
+      <div className='box'>
+        <h2>Titre</h2>
+      </div>
     )
   }
 }
 
-export default App
+export default App;
---
+++ src/components/Connexion.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+
+class Connexion extends Component {
+  state = {
+    pseudo: '',
+    goToChat: false
+  }
+
+  handleChange = event => {
+    console.log('handleChange')
+    const pseudo = event.target.value
+
+    this.setState({ pseudo })
+  }
+
+  handleSubmit = event => {
+    console.log('handleSubmit')
+
+    event.preventDefault()
+    this.setState({ goToChat: true })
+  }
+
+  render () {
+    if (this.state.goToChat) {
+      return <Redirect to={`/pseudo/${this.state.pseudo}`} />
+    }
+
+    return (
+      <div className='connexionBox'>
+        <form className='connexion' onSubmit={this.handleSubmit}>
+          <input 
+            type='text' placeholder='Pseudo' required
+            value={this.state.pseudo}
+            onChange={this.handleChange} />
+          <button type='submit'>GO</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Connexion
